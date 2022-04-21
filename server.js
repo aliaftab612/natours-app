26001d42e083dd0c1762f9f9dc4d23d3a8f20d51
Aftab Ali <67967050+aliaftab612@users.noreply.github.com>
2022-04-21 20:10:03 +0530

    ki

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose
     console.log(`Connected Successfully to MongoDB`);
   });
 
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 const server = app.listen(port, () => {
   console.log(`Server listening on Port ${port}`);
 });
