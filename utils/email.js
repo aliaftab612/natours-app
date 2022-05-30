92367bc2aad9a2042475cda179face07281b3b63
Aftab Ali <67967050+aliaftab612@users.noreply.github.com>
2022-05-30 21:39:19 +0530

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -1,23 +1,54 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  var transport = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-      user: 'e5717cfc59a360',
-      pass: '90e3cd67268e14',
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.user = user;
+    this.url = url;
+    this.firstName = user.name.split(' ')[0];
+  }
 
-  const mailOptions = {
-    from: 'Natours <noreply@natours.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+  createTransport() {
+    return nodemailer.createTransport({
+      host: 'smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: 'e5717cfc59a360',
+        pass: '90e3cd67268e14',
+      },
+    });
+  }
 
-  await transport.sendMail(mailOptions);
+  async sendEmail(template, subject) {
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        title: subject,
+        firstName: this.firstName,
+        url: this.url,
+      }
+    );
+
+    const mailOptions = {
+      from: 'Natours <noreply@natours.io>',
+      to: this.user.email,
+      subject,
+      html,
+      text: htmlToText.htmlToText(html),
+    };
+
+    await this.createTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcomeEmail() {
+    await this.sendEmail('welcome', 'Welcome to the Natours family!!');
+  }
+
+  async sendResetPasswordEmail() {
+    await this.sendEmail(
+      'resetPassword',
+      'Reset Link Natours Password (Expires in 10 Mins)'
+    );
+  }
 };
-
-module.exports = sendEmail;
